178a6bdea43b2fe743d2d5b89b61d724a5dd3262
mesflit <84642884+mesflit@users.noreply.github.com>
2023-04-14 18:47:36 +0000

    Create gentoo.sh

---
+++ gentoo.sh
@@ -0,0 +1,14 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Gentoo"
+DISTRO_COMMENT="github.com/mesflit"
+
+TARBALL_URL['aarch64']="https://ftp.dimensiondata.com/linux/distributions/gentoo/releases/arm64/autobuilds/20230409T234655Z/stage3-arm64-openrc-20230409T234655Z.tar.xz"
+TARBALL_SHA256['aarch64']="2335803a86a4af3c03378b5a4e286059429c6f152d233e6564a35177aa4c55f5"
+TARBALL_URL['arm']="https://ftp.dimensiondata.com/linux/distributions/gentoo/releases/arm/autobuilds/20230412T210254Z/stage3-armv7a-openrc-20230412T210254Z.tar.xz"
+TARBALL_SHA256['arm']="611a69443d1007b082ab58387790a1ea0d696a5f316131e6a2b4e4979fc68866"
+TARBALL_URL['i686']="https://ftp.dimensiondata.com/linux/distributions/gentoo/releases/x86/autobuilds/20230410T170202Z/stage3-i686-openrc-20230410T170202Z.tar.xz"
+TARBALL_SHA256['i686']="f473bad1dda6640a7892629495f18a2a29e04af03d3ba9d59663e1c3cce5cbe7"
+TARBALL_URL['x86_64']="https://ftp.dimensiondata.com/linux/distributions/gentoo/releases/amd64/autobuilds/20230409T163155Z/stage3-amd64-openrc-20230409T163155Z.tar.xz"
+TARBALL_SHA256['x86_64']="9c2953c64c2297623f5042da9698745e049b16048c64394514e57b417643d91d"
