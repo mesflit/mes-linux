bdc5c2ff70bf71cfbe4267cb4b6c5a77f0af7e4c
mesflit <84642884+mesflit@users.noreply.github.com>
2023-07-12 07:36:41 +0300

    Update gentooinstaller.sh

--- gentooinstaller.sh
+++ gentooinstaller.sh
@@ -1,12 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
+rm -rf $PREFIX/etc/proot-distro/gentoo.sh;
 apt update;
  apt install proot-distro;
  wget https://raw.githubusercontent.com/mesflit/proot-distro/main/gentoo.sh;
 
 mv gentoo.sh $PREFIX/etc/proot-distro/gentoo.sh;
+proot-distro install gentoo;
 clear; 
 echo " Added Gentoo
-'proot-distro install gentoo'
-you can download by typing
+
 'proot-distro login gentoo'
 You can start by typing "
