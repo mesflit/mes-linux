7ec7ea15e39f6e36de99ca4531d2fd09691d5fe0
mesflit <84642884+mesflit@users.noreply.github.com>
2023-07-12 07:34:58 +0300

    Update kaliinstaller.sh

--- kaliinstaller.sh
+++ kaliinstaller.sh
@@ -1,12 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
+rm -rf $PREFIX/etc/proot-distro/kali.sh;
 apt update;
  apt install proot-distro;
  wget https://raw.githubusercontent.com/mesflit/proot-distro/main/kali.sh;
 
+
 mv kali.sh $PREFIX/etc/proot-distro/kali.sh;
+proot-distro install kali;
 clear; echo "Added Kali Linux
-'proot-distro install kali'
-you can download by typing
+
 'proot-distro login kali'
 You can start by typing
 NOTE: After booting kali linux on first download type this command 'rm -rf /var/lib/dpkg/info/postgresql* && dpkg --configure -a' you won't get an error";
