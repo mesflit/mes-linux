6bb86c0d6d3a27de799d572783fdd30122be6b78
mesflit <84642884+mesflit@users.noreply.github.com>
2023-07-27 22:47:15 +0300

    Update voidglibcinstaller.sh

--- voidglibcinstaller.sh
+++ voidglibcinstaller.sh
@@ -4,7 +4,7 @@ apt update;
  apt install proot-distro;
  wget https://raw.githubusercontent.com/mesflit/proot-distro/main/voidglibc.sh;
 
-mv gentoo.sh $PREFIX/etc/proot-distro/voidglibc.sh;
+mv voidglibc.sh $PREFIX/etc/proot-distro/voidglibc.sh;
 proot-distro install voidglibc;
 clear; 
 echo " Added Void Glibc
