86b5b9c41e9f06a52be7798e673e94023e248a5c
mesflit <84642884+mesflit@users.noreply.github.com>
2023-07-27 07:23:37 +0300

    Create voidglibc.sh

---
+++ voidglibc.sh
@@ -0,0 +1,14 @@
+# This is a default distribution plug-in. 
+ # Do not modify this file as your changes will be overwritten on next update. 
+ # If you want customize installation, please make a copy. 
+ DISTRO_NAME="Void Linux Glibc" 
+ DISTRO_COMMENT="Glibc" 
+  
+ TARBALL_URL['aarch64']="https://github.com/mesflit/proot-distro/releases/download/void-2023-06-28-glibc/void-aarch64-ROOTFS-20230628.tar.xz" 
+ TARBALL_SHA256['aarch64']="d6d10addd669d4d458e57ea38fc73086aedf911b6d702e5b4ea97cade08cbc6f"
+ TARBALL_URL['arm']="https://github.com/mesflit/proot-distro/releases/download/void-2023-06-28-glibc/void-armv7l-ROOTFS-20230628.tar.xz" 
+ TARBALL_SHA256['arm']="e28203312278c12c2d5a082f22a9f83e422fcd4277d43a9b18bdc3a9394f472a"
+ TARBALL_URL['i686']="https://github.com/mesflit/proot-distro/releases/download/void-2023-06-28-glibc/void-i686-ROOTFS-20230628.tar.xz" 
+ TARBALL_SHA256['i686']="6784299948c09754e65ece7c939f4d4384b4d1c60d2c96bbd5872644c6a92916"
+ TARBALL_URL['x86_64']="https://github.com/mesflit/proot-distro/releases/download/void-2023-06-28-glibc/void-x86_64-ROOTFS-20230628.tar.xz" 
+ TARBALL_SHA256['x86_64']="8e20003b663bc4a9c4dbe3383e3ac94a7bcf051e47f433c63c24bd639ca19334"
