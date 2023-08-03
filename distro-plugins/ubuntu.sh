dac2d818cb129a66933a8a37b9420497b164d620
mesflit <84642884+mesflit@users.noreply.github.com>
2023-08-03 17:09:55 +0300

    Delete ubuntu.sh

--- distro-plugins/ubuntu.sh
+++
@@ -1,7 +0,0 @@
-DISTRO_NAME="Ubuntu 23.04"
-TARBALL_URL['aarch64']="https://cdimage.ubuntu.com/ubuntu-base/releases/23.04/release/ubuntu-base-23.04-base-arm64.tar.gz"
-TARBALL_SHA256['aarch64']="2281fd343e31cd3deafcc4263315059770f374babbdb0436dbe4ae576cf8e269"
-TARBALL_URL['arm']="https://cdimage.ubuntu.com/ubuntu-base/releases/23.04/release/ubuntu-base-23.04-base-armhf.tar.gz"
-TARBALL_SHA256['arm']="4f64b649585a4c2f5895c402018ec24e63ad2532287b66202e9a63aab430e34a"
-TARBALL_URL['x86_64']="https://cdimage.ubuntu.com/ubuntu-base/releases/23.04/release/ubuntu-base-23.04-base-amd64.tar.gz"
-TARBALL_SHA256['x86_64']="08fcb72dc556685b62aa2a70bc5bdcbccf8dd866917c473dd87dc7235f6567b7"
